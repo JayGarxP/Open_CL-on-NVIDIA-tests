3673833c3d79931f3bb1856a23106bb606742d3d
JayGarxP <x>
2018-08-13 16:07:09 -0700

    GPU vendor check working! basic_string to std::string tricky, can't use compare or ==; need to use char by char comparison. Code running on gpu!

--- openCL_hellowWorld/OpenCL_helloWorld/OpenCL_arrayProcessing/OpenCLHelper.cpp
+++ openCL_hellowWorld/OpenCL_helloWorld/OpenCL_arrayProcessing/OpenCLHelper.cpp
@@ -11,15 +11,32 @@ cl::Program CreateProgram(const std::string &file)
 
 	_ASSERT(platforms.size() > 0);
 
+	std::string INTEL_PLATFORM_NAME("Intel(R) OpenCL");
+	// char word[] = "Intel(R) OpenCL"; basic_string<allocator> not playing nicely with \0 null character
+	// basic_string.compare(PLATFORM) == 0 also not working, makes no sense, can't find info about this
+	std::string NVIDIA_PLATFORM_NAME("NVIDIA CUDA");
 	// Range-based for loop to iterate through the array of platforms.  
 	for (auto platform : platforms) {   
 		auto test = platform.getInfo<CL_PLATFORM_NAME>();
-		
+
+		//if Intel, for all device(s) = intel "Intel(R) OpenCL" 
+		if (test[0] == INTEL_PLATFORM_NAME[0]) 
+		{
+			std::cout << "INTEL HIT\n";
+			//save cpu device here if you want to in future
+		}
+		else if (test[0] == NVIDIA_PLATFORM_NAME[0]) 
+		{
+			std::cout << "NVIDIA GPU HiiT\n";
+
+			platform.getDevices(CL_DEVICE_TYPE_GPU, &nvidia_devices);
+		}
+		else { std::cout << "BZZZT! No intel or nvidia devices found on any platforms"; }
+		//if Nvidia, for all Nvidia device(s) = nvidia device reference "Intel(R) OpenCL" "NVIDIA CUDA"
 		
 	}
 		
 	
-
 	std::ifstream helloWorldFile("HelloWorld.cl");
 
 	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
@@ -28,10 +45,12 @@ cl::Program CreateProgram(const std::string &file)
 	cl::Program::Sources sources(1, std::make_pair(src.c_str(), strlen(src.c_str())));
 
 
-	//cl::Context context(REAL_GPU_device);
-	//cl::Program program(context, sources);
+	auto REAL_GPU_device = nvidia_devices.front();
+	cl::Context context(REAL_GPU_device);
+	cl::Program program(context, sources);
 
-	//auto buildError = program.build("-cl-std=CL1.2");
-	cl::Program emptyProgram(NULL, sources);
-	return emptyProgram;
+	auto buildError = program.build("-cl-std=CL1.2");
+	//cl::Program emptyProgram(NULL, sources);
+	//return emptyProgram;
+	return program;
 }
--- openCL_hellowWorld/OpenCL_helloWorld/OpenCL_arrayProcessing/OpenCL_arrayProcessing.cpp
+++ openCL_hellowWorld/OpenCL_helloWorld/OpenCL_arrayProcessing/OpenCL_arrayProcessing.cpp
@@ -7,23 +7,38 @@
 int main()
 {
 	
-	CreateProgram("HelloWorld.cl");
+	auto program  = CreateProgram("HelloWorld.cl");
+	auto context = program.getInfo<CL_PROGRAM_CONTEXT>(); 
+	auto devices = context.getInfo<CL_CONTEXT_DEVICES>();
+	//Test if vendor is intel or nvidia
+	/*
+	// Range-based for loop to iterate through the array of platforms.  
+	for (auto platform : platforms) {   
+		auto test = platform.getInfo<CL_PLATFORM_NAME>();
+		//if Intel, for all device(s) = intel
+		//if Nvidia, for all Nvidia device(s) = nvidia device reference 
+		
+	}
+	*/
+	//'&' address reference to front device; //https://stackoverflow.com/questions/13230480/what-does-auto-tell-us
+	auto& device = devices.front(); //prolly intel
 
-//char buf[16];
-//cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
-//cl::Kernel kernel(program, "HelloWorld", &buildError);
-//
-//kernel.setArg(0, memBuf);
-//
-//
+char buf[16];
+cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
+cl::Kernel kernel(program, "HelloWorld");
+
+kernel.setArg(0, memBuf);
+
+
 //cl::CommandQueue queue(context, REAL_GPU_device);
-//queue.enqueueTask(kernel); //execute task
-//
-////read back in; bool blocking true means WAIT until done (pause execution)
-////Read from global memory of device to memory on our side
-//queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
-//    
-//std::cout << "\n\n\n" << buf;
+cl::CommandQueue queue(context, device);
+queue.enqueueTask(kernel); //execute task
+
+//read back in; bool blocking true means WAIT until done (pause execution)
+//Read from global memory of device to memory on our side
+queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+    
+std::cout << "\n\n\n" << buf;
 std::cin.get();
 
 return 0;
